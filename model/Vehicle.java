15940e6630c2d0b4f3317c9ba29651905038c702
BusayoAyotunde <202203768@ub.ac.bw>
2026-04-17 22:00:05 +0200

    Refined Vehicle and Car classes and got rid of the errors

---
+++ model/Vehicle.java
@@ -0,0 +1,53 @@
+package outdrive.model;
+
+public abstract class Vehicle{  //fields
+    private String id;
+    private String name;
+    private double price;
+    private boolean isAvailable;
+
+public Vehicle( String id, String name, double price, boolean isAvailable){   //constructor
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.isAvailable= isAvailable;
+
+}
+//getters:
+public String getId(){
+    return id;
+}
+
+public String getName(){
+    return name;
+}
+
+public double getPrice(){
+    return price;
+}
+public boolean isAvailable() {
+    return isAvailable;
+    }
+
+//setters:
+ public void setId(String id) {
+    this.id = id; 
+}
+
+public void setName(String name) {
+    this.name = name; 
+}
+
+  public void setPrice(double price) { 
+    this.price = price; 
+}
+
+public void setIsAvailable(boolean available){
+    isAvailable = available;
+}
+
+
+ public abstract double calculateRentalCost(int days);  //method to calculate charge of renting vehicles-abstract
+     
+ 
+}
